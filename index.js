45d9b299dacc296d9b820bc0a75349f4e2c0f990
Meliodas <79414899+Meliodas-rai@users.noreply.github.com>
2021-05-22 19:48:12 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -948,44 +948,6 @@ if (text.includes("placa"))
 					membr.push(otakus10.jid)
 					mentions(teks, membr, true)
 						break
-					case 'otakus':
-					if (!isGroup) return reply(`Comando so pode ser utiizado em grupos parsa`)
-					membr = []
-					const gado1 = groupMembers
-					const gado2 = groupMembers
-					const gado3 = groupMembers
-					const gado3 = groupMembers
-					const gado5 = groupMembers
-					const gado6 = groupMembers
-					const gado7 = groupMembers
-					const gado8 = groupMembers
-					const gado9 = groupMembers
-					const gado10 = groupMembers
-					const gados1 = gado1[Math.floor(Math.random() * gado1.length)]
-					const gados2 = gado2[Math.floor(Math.random() * gado2.length)]
-					const gados3 = gado3[Math.floor(Math.random() * gado3.length)]
-					const gados4 = gado4[Math.floor(Math.random() * gado4.length)]
-					const gados5 = gado5[Math.floor(Math.random() * gado5.length)]
-					const gados6 = gado6[Math.floor(Math.random() * gado6.length)]
-					const gados7 = gado7[Math.floor(Math.random() * gado7.length)]
-					const gados8 = gado8[Math.floor(Math.random() * gado8.length)]
-					const gados9 = gado9[Math.floor(Math.random() * gado9.length)]
-					const gados10 = gado10[Math.floor(Math.random() * gado10.length)]
-					var pct = ["1%", `2%`, `3%`, `4%`, `5%`, `6%`, `7`, `8%`, `9%`, `10`, `11%`, `12%`,`13%`, `14%`, `15%`, `16%`, `17%`, `18%`, `19%`, `20%`, `21%`, `22`, `23%`, `24%`, `25%`, `26%`, `27%`, `28%`, `27%`, `28%`, `29%`, `30%`, `31%`, `32%`, `33%`, `34%`, `35%`, `36%`, `37%`, `38%`, `39%`, `40%`, `41%`, `42%`, `43%`, `44%`, `45%`, `46%`, `47%`, `48%`, `49%`, `50%`, `51%`, `52%`, `53%`, `54%`, `55%`, `56%`, `57%`, `58%`, `59%`, `60%`, `61%`, `62%`, `63%`, `64%`, `65%`, `66%`, `67%`, `68%`, `69%`, `70%`, `71%`, `72%`, `73%`, `74%`, `75%`, `76%`, `77%`, `78%`, `79%`, `80%`, `81%`, `82%`, `85%`, `84%`, `85%`, `86%`, `87%`, `88%`, `89%`, `90%`, `91%`, `92%`, `93%`, `94%`, `95%`, `96%`, `97%`, `98%`, `99%`, `100%`]
-					const porcentagem = pct[Math.floor(Math.random() * pct.length)]
-					teks = `${pushname} Esses são os mais Gados do grupo ${groupName}\n@${gados1.jid.split('@')[0]}\nCom uma porcentagem de ${porcentagem}\n@${gados2.jid.split('@')[0]}\nCom uma porcentagem de ${porcentagem}\n@${gados3.jid.split('@')[0]}\nCom uma porcentagem de ${porcentagem}\n@${gados4.jid.split('@')[0]}\nCom uma porcentagem de ${porcentagem}\n@${gados5.jid.split('@')[0]}\nCom uma porcentagem de ${porcentagem}\n@${gados6.jid.split('@')[0]}\nCom uma porcentagem de ${porcentagem}\n@${gados7.jid.split('@')[0]}\nCom uma porcentagem de ${porcentagem}\n@${gados8.jid.split('@')[0]}\nCom uma porcentagem de ${porcentagem}\n@${gados9.jid.split('@')[0]}\nCom uma porcentagem de ${porcentagem}\n@${gados10.jid.split('@')[0]}\nCom uma porcentagem de ${porcentagem} Sem pressão Neném, ⚡Xandão⚡ ta aqui`
-					membr.push(gados1.jid)
-					membr.push(gados2.jid)
-					membr.push(gados3.jid)
-					membr.push(gados4.jid)
-					membr.push(gadps5.jid)
-					membr.push(gados6.jid)
-					membr.push(gados7.jid)
-					membr.push(gados8.jid)
-					membr.push(gados9.jid)
-					membr.push(gados10.jid)
-					mentions(teks, membr, true)
-						break
 				case 'install':
 			reply(`Aqui os comandos do ⚡Super Xandão⚡ ${pushname}`)
 			setTimeout( () => {
