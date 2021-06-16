4a701b0721d7820e9226a82d3886a56c0e580552
Meliodas <79414899+Meliodas-rai@users.noreply.github.com>
2021-06-16 11:12:50 -0300

    Add files via upload

--- install.sh
+++ install.sh
@@ -16,19 +16,6 @@ pkg install sl
 pkg install figlet -y
 mv Utils.js /data/data/com.termux/files/home/Bot/node_modules/@adiwajshing/baileys/lib/WAConnection/
 echo "Pronto, o nécessario ja foi instalado, irei iniciar o bot em 15 segundos" |lolcat -a -d 19
-clear
-echo "10"
-echo "9"
-echo "8"
-echo "7"
-echo "6"
-echo "5"
-echo "4"
-echo "3"
-echo "2"
-echo "1"
-clear
 figlet "Zeus" |lolcat 
 echo "Eu SOU ⚡ ZEUS ⚡ " |lolcat -a -d 23
-clear
 node index.js
