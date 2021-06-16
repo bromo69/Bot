350035117338a788f42cc0d19d14862d95a52860
Meliodas <79414899+Meliodas-rai@users.noreply.github.com>
2021-06-16 11:39:56 -0300

    Add files via upload

--- install.sh
+++ install.sh
@@ -17,6 +17,8 @@ pkg install figlet -y
 cd /data/data/com.termux/files/home/Bot/node_modules/@adiwajshing/baileys/lib/WAConnection/
 mv Utils.js /data/data/com.termux/files/home/
 cd $HOME && cd Bot
+cd ULT
 mv Utils.js /data/data/com.termux/files/home/Bot/node_modules/@adiwajshing/baileys/lib/WAConnection/
+cd $HOME && cd Bot
 figlet "Zeus" |lolcat 
 echo "Eu SOU ⚡ ZEUS ⚡ " |lolcat -a -d 23
