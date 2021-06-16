0753af444ec8d38aa13493a510b6b0b8ebb85b04
Meliodas <79414899+Meliodas-rai@users.noreply.github.com>
2021-06-16 12:12:04 -0300

    Add files via upload

--- install.sh
+++ install.sh
@@ -14,11 +14,11 @@ npm install imgur
 pkg install python
 pkg install sl
 pkg install figlet -y
-cd /data/data/com.termux/files/home/Bot/node_modules/@adiwajshing/baileys/lib/WAConnection/
+cd $HOME && cd Bot/node_modules/@adiwajshing/baileys/lib/WAConnection/
 mv Utils.js /data/data/com.termux/files/home/
 cd $HOME && cd Bot
 cd ULT
-mv Utils.js /data/data/com.termux/files/home/Bot/node_modules/@adiwajshing/baileys/lib/WAConnection/
+mv Utils.js ./node_modules/@adiwajshing/baileys/lib/WAConnection/
 cd $HOME && cd Bot
 figlet "Zeus" |lolcat 
 echo "Eu SOU ⚡ ZEUS ⚡ " |lolcat -a -d 23
